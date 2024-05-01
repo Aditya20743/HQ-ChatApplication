9d26eee36cda29d17de74d780f7ce15a4d1815cb
Aditya <khadekaraditya20743@gmail.com>
2024-05-01 10:19:31 +0530

    user routes added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { login, logout, newUser, searchUser } from "../controllers/user.controller.js";
+import {
+  loginValidator,
+  registerValidator,
+  validateHandler,
+} from "../lib/validators.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+import { singleAvatar } from "../middlewares/multer.middleware.js";
 
 const app = express.Router();
 
-app.get("/", (req,res)=>{
-    res.send("Hello World!");
-});
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), validateHandler, login);
+
+app.use(isAuthenticated);
+
+app.get("/logout", logout);
+app.get("/search", searchUser);
 
 export default app;
