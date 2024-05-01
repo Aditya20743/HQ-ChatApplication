b7ef2d28790e6eb32f3872cb1cc3dd1680a2cc8d
Aditya <khadekaraditya20743@gmail.com>
2024-05-01 10:17:30 +0530

    helpers and validator added

---
+++ server/lib/validator.js
@@ -0,0 +1,45 @@
+import { body, param, validationResult } from "express-validator";
+import { ErrorHandler } from "../utils/utility.js";
+
+const validateHandler = (req, res, next) => {
+  const errors = validationResult(req);
+
+  const errorMessages = errors
+    .array()
+    .map((error) => error.msg)
+    .join(", ");
+
+  if (errors.isEmpty()) return next();
+  else next(new ErrorHandler(errorMessages, 400));
+};
+
+const registerValidator = () => [
+  body("name", "Please Enter Name").notEmpty(),
+  body("username", "Please Enter Username").notEmpty(),
+  body("password", "Please Enter Password").notEmpty(),
+];
+
+const loginValidator = () => [
+  body("username", "Please Enter Username").notEmpty(),
+  body("password", "Please Enter Password").notEmpty(),
+];
+
+const sendAttachmentsValidator = () => [
+  body("chatId", "Please Enter Chat ID").notEmpty(),
+];
+
+const chatIdValidator = () => [param("id", "Please Enter Chat ID").notEmpty()];
+
+const renameValidator = () => [
+  param("id", "Please Enter Chat ID").notEmpty(),
+  body("name", "Please Enter New Name").notEmpty(),
+];
+
+export {
+  chatIdValidator,
+  loginValidator,
+  registerValidator,
+  renameValidator,
+  sendAttachmentsValidator,
+  validateHandler,
+};
